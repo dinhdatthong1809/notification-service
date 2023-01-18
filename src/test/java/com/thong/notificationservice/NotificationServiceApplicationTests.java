32867a09c30c6a6f5193b3750ad2aa11ad5777b0
thong.dinh <datthong180997@gmail.com>
2023-01-18 11:45:55 +0700

    first commit

---
+++ src/test/java/com/thong/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thong.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
